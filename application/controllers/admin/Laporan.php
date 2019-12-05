5d4796f473fbc408c1931c44707e73cefb034e71
youwantorefo <44155450+youwantorefo@users.noreply.github.com>
2019-12-05 22:28:21 +0700

    laporan
    laporan

--- application/controllers/admin/Laporan.php
+++ application/controllers/admin/Laporan.php
@@ -1,20 +1,29 @@
 <?php
-  class Home extends CI_Controller{
-    public function __construct(){
-      parent::__construct();
-       // if($this->session->userdata('status') != "login"){
-
-       //       redirect(base_url("login"));
-       //   }else{
-       //        if($this->session->userdata('tabel_admin') != "username"){
-       //           echo"anda bukan admin";
-       //           redirect(base_url("login"));
-       //       }
-       //   }
-    }
-
-    public function index(){
-      $this->load->view('admin/laporan.php');
-    }
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Laporan extends CI_Controller
+{
+ 
+  function __construct()
+  {
+    parent::__construct();    
+    $this->load->model('admin/laporan_model');
+  $this->load->library('form_validation');
   }
- ?>
+ 
+  function index()
+  {
+    $bulan = $this->input->post('bulan');
+    $tahun = $this->input->post('tahun');
+    if ($bulan  == null && $tahun == null) {
+      $data['laporan'] = $this->laporan_model->getlaporan_transaksi();
+      # code...
+    }else{
+      $data['laporan'] = $this->laporan_model->getlaporan_transaksi_bulan($bulan,$tahun);
+      
+    }
+    $data['transaksi'] = $this->laporan_model->getdate_year();
+    $this->load->view('admin/laporan/list',$data);
+
+  }
+}
+?>
