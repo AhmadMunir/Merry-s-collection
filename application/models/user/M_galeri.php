e58c9116e135abfe47b2b7d1a43d19fd33df1208
adesetiawan <44155091+adeas10@users.noreply.github.com>
2019-12-05 23:40:38 +0700

    ASDFGH

---
+++ application/models/user/M_galeri.php
@@ -0,0 +1,11 @@
+<!-- <?php defined('BASEPATH') OR exit ('No direct script access allowed');
+/**
+ *
+ */
+class M_galeri extends CI_Model
+{
+  public function load_banner(){
+    return $this->db->get('tabel_galeri')->result();
+  }
+}
+?> -->
