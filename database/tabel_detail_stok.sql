6daebdadb751f2f01b25ce33b47b073ae981444b
youwantorefo <44155450+youwantorefo@users.noreply.github.com>
2019-12-06 09:53:53 +0700

    safas
    faf

--- database/tabel_detail_stok.sql
+++ database/tabel_detail_stok.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 13, 2019 at 08:54 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.3
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Des 2019 pada 03.53
+-- Versi server: 10.1.36-MariaDB
+-- Versi PHP: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tabel_detail_stok`
+-- Struktur dari tabel `tabel_detail_stok`
 --
 
 CREATE TABLE `tabel_detail_stok` (
@@ -37,17 +37,20 @@ CREATE TABLE `tabel_detail_stok` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tabel_detail_stok`
+-- Dumping data untuk tabel `tabel_detail_stok`
 --
 
 INSERT INTO `tabel_detail_stok` (`id_detail_stok`, `id_barang`, `size`, `deskripsi`, `jumlah_stok`) VALUES
 (9, 'I9qfM', '1', '1', 4),
 (10, 'Hl025', '38', '-', 19),
 (11, 'Hl025', '39', '-', 2),
-(12, 'Hl025', '40', '-', 8);
+(12, 'Hl025', '40', '-', 8),
+(13, 'p5KI4', 'l', 'sd', 10),
+(14, '59lzd', 'l', 'sdf', 12),
+(15, 'DJOd1', 'l', 'hh', 90);
 
 --
--- Triggers `tabel_detail_stok`
+-- Trigger `tabel_detail_stok`
 --
 DELIMITER $$
 CREATE TRIGGER `stok_barang` AFTER INSERT ON `tabel_detail_stok` FOR EACH ROW UPDATE tabel_barang
@@ -67,31 +70,21 @@ DELIMITER ;
 --
 
 --
--- Indexes for table `tabel_detail_stok`
+-- Indeks untuk tabel `tabel_detail_stok`
 --
 ALTER TABLE `tabel_detail_stok`
   ADD PRIMARY KEY (`id_detail_stok`),
   ADD KEY `id_barang` (`id_barang`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tabel_detail_stok`
+-- AUTO_INCREMENT untuk tabel `tabel_detail_stok`
 --
 ALTER TABLE `tabel_detail_stok`
-  MODIFY `id_detail_stok` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tabel_detail_stok`
---
-ALTER TABLE `tabel_detail_stok`
-  ADD CONSTRAINT `tabel_detail_stok_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tabel_barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id_detail_stok` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
