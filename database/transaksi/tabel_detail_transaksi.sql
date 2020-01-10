056432734859241f379463a78efa9d5ab19aadde
munir <hel000941@gmail.com>
2020-01-10 23:12:30 +0700

    tbltransaksibaru

---
+++ database/transaksi/tabel_detail_transaksi.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 10, 2020 at 05:05 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `merry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabel_detail_transaksi`
+--
+
+CREATE TABLE `tabel_detail_transaksi` (
+  `id_detail_transaksi` varchar(5) NOT NULL,
+  `id_transaksi` varchar(17) NOT NULL,
+  `id_user` varchar(7) NOT NULL,
+  `id_barang` varchar(5) NOT NULL,
+  `qty` int(3) NOT NULL,
+  `subtotal` smallint(6) NOT NULL,
+  `tax` float NOT NULL,
+  `tanggal` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tabel_detail_transaksi`
+--
+
+INSERT INTO `tabel_detail_transaksi` (`id_detail_transaksi`, `id_transaksi`, `id_user`, `id_barang`, `qty`, `subtotal`, `tax`, `tanggal`) VALUES
+('S9gR0', '3CA304653R4228947', '1', 'ywCpL', 1, 20, 0.88, '2020-01-10 23:05:04');
+
+--
+-- Triggers `tabel_detail_transaksi`
+--
+DELIMITER $$
+CREATE TRIGGER `add_total_trans` AFTER INSERT ON `tabel_detail_transaksi` FOR EACH ROW UPDATE tabel_transaksi
+SET total=(SELECT SUM(subtotal) 
+           from tabel_detail_transaksi
+where tabel_transaksi.id_transaksi= tabel_detail_transaksi.id_transaksi)
+    where tabel_transaksi.id_transaksi = New.id_transaksi
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_stok_trans` AFTER UPDATE ON `tabel_detail_transaksi` FOR EACH ROW UPDATE tabel_transaksi
+SET total=(SELECT SUM(subtotal) 
+           from tabel_detail_transaksi
+where tabel_transaksi.id_transaksi= tabel_detail_transaksi.id_transaksi)
+    where tabel_transaksi.id_transaksi = New.id_transaksi
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tabel_detail_transaksi`
+--
+ALTER TABLE `tabel_detail_transaksi`
+  ADD PRIMARY KEY (`id_detail_transaksi`),
+  ADD KEY `id_barang` (`id_barang`),
+  ADD KEY `s` (`id_transaksi`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tabel_detail_transaksi`
+--
+ALTER TABLE `tabel_detail_transaksi`
+  ADD CONSTRAINT `id_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `tabel_transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tabel_detail_transaksi_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tabel_barang` (`id_barang`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ database/transaksi/tabel_transaksi.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 10, 2020 at 05:05 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `merry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabel_transaksi`
+--
+
+CREATE TABLE `tabel_transaksi` (
+  `id_transaksi` varchar(17) NOT NULL,
+  `id_user` int(7) NOT NULL,
+  `tanggal` datetime NOT NULL DEFAULT current_timestamp(),
+  `total` float DEFAULT NULL,
+  `tax_total` float NOT NULL,
+  `shipping` float NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `alamat_pengiriman` text NOT NULL,
+  `shipping_method` varchar(20) NOT NULL,
+  `paypal_fee` float NOT NULL,
+  `terima_bersih` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tabel_transaksi`
+--
+
+INSERT INTO `tabel_transaksi` (`id_transaksi`, `id_user`, `tanggal`, `total`, `tax_total`, `shipping`, `status`, `alamat_pengiriman`, `shipping_method`, `paypal_fee`, `terima_bersih`) VALUES
+('3CA304653R4228947', 1, '2020-01-10 23:05:03', 20, 0.88, 4, 'Order Accepted', 'Rt 9  Rw 4 Sukosari Lor, Kabupaten Kaur, Bengkulu, AS', 'JNE', 1.02, 23.86);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tabel_transaksi`
+--
+ALTER TABLE `tabel_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tabel_transaksi`
+--
+ALTER TABLE `tabel_transaksi`
+  ADD CONSTRAINT `tabel_transaksi_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tabel_user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
