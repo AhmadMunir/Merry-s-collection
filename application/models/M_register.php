8327fe40999ddcc7c293c83b9ce44419bf9f2315
youwantorefo <44155450+youwantorefo@users.noreply.github.com>
2020-01-12 17:39:29 +0700

    register admin
    register admin

--- application/models/M_register.php
+++ application/models/M_register.php
@@ -2,11 +2,22 @@
 class M_register extends CI_Model
 {
 	private $table = "tabel_user";
+	private $_table= "tabel_admin";
 
 	function cek_register($table,$where)
 	{
 		return $this->db->get_where($table,$where);
 	}
+
+	function cek_registerr($table,$where)
+	{
+		return $this->db->get_where($_table,$where);
+	}
+
+	public function simpan($register)
+    {
+        $this->db->insert("tabel_admin", $register);
+    }
 
 	public function save($register)
     {
