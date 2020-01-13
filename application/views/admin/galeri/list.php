248e12478244294c61e0ec4b886fad392129bf92
ridiyoga <44762924+ridiyoga@users.noreply.github.com>
2020-01-13 10:22:22 +0700

    pelanggan

--- application/views/admin/galeri/list.php
+++ application/views/admin/galeri/list.php
@@ -55,54 +55,54 @@
                                     <table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true" data-key-events="true" data-show-toggle="true" data-resizable="true" data-cookie="true"
                                         data-cookie-id-table="saveId" data-show-export="true" data-click-to-select="true" data-toolbar="#toolbar">
                                         <thead>
-                                <tr>
-                                  <th>No</th>
-                                  <th>Judul Galeri</th>
-                                  <th>Gambar 1</th>
-                                  <th>Gambar 2</th>
-                                  <th>Gambar 3</th>
-                                  <th>Gambar 4</th>
-                                  <th>Gambar 5</th>
-                                  <th>Tindakan</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <?php $i=1?>
-                                <?php foreach ($tabel_galeri as $galeri): ?>
                                   <tr>
-                                    <td width="10">
-                                      <?php echo $i++?>
-                                    </td>
-                                    <td width="150">
-                                      <?php echo $galeri->judul ?>
-                                    </td>
-                                     <td width="150">
-                                      <img src="<?php echo base_url('img/galeri/'.$galeri->gambar) ?>" style="width: 70px;height: 70px;" />
-                                    </td> 
-                                    <td width="150">
-                                      <img src="<?php echo base_url('img/galeri/'.$galeri->gambar2) ?>" style="width: 70px;height: 70px;" />
-                                    </td> 
-                                    <td width="150">
-                                      <img src="<?php echo base_url('img/galeri/'.$galeri->gambar3) ?>" style="width: 70px;height: 70px;" />
-                                    </td> 
-                                    <td width="150">
-                                      <img src="<?php echo base_url('img/galeri/'.$galeri->gambar4) ?>" style="width: 70px;height: 70px;" />
-                                    </td> 
-                                    <td width="150">
-                                      <img src="<?php echo base_url('img/galeri/'.$galeri->gambar5) ?>" style="width: 70px;height: 70px;" />
-                                    </td> 
-                                    
-                                    <td width="200">
-                                    <a href="<?php echo site_url('admin/galeri/edit/'.$galeri->id_galeri)?>" class="btn btn-info">Edit</a>
-                                 
-                                    <a onclick="deleteConfirm('<?php echo site_url('admin/galeri/delete/'.$galeri->id_galeri)?>')"
-                                    href="#!" class="btn btn-primary">Hapus</a>
-                                    </td>
+                                    <th>No</th>
+                                    <th>Judul Galeri</th>
+                                    <th>Gambar 1</th>
+                                    <th>Gambar 2</th>
+                                    <th>Gambar 3</th>
+                                    <th>Gambar 4</th>
+                                    <th>Gambar 5</th>
+                                    <th>Tindakan</th>
                                   </tr>
-                                <?php endforeach; ?>
-                              </tbody>
-                            </table>
-                          </div>
+                                </thead>
+                                <tbody>
+                                  <?php $i=1?>
+                                  <?php foreach ($tabel_galeri as $galeri): ?>
+                                    <tr>
+                                      <td width="10">
+                                        <?php echo $i++?>
+                                      </td>
+                                      <td width="150">
+                                        <?php echo $galeri->judul ?>
+                                      </td>
+                                       <td width="150">
+                                        <img src="<?php echo base_url('img/galeri/'.$galeri->gambar) ?>" style="width: 70px;height: 70px;" />
+                                      </td> 
+                                      <td width="150">
+                                        <img src="<?php echo base_url('img/galeri/'.$galeri->gambar2) ?>" style="width: 70px;height: 70px;" />
+                                      </td> 
+                                      <td width="150">
+                                        <img src="<?php echo base_url('img/galeri/'.$galeri->gambar3) ?>" style="width: 70px;height: 70px;" />
+                                      </td> 
+                                      <td width="150">
+                                        <img src="<?php echo base_url('img/galeri/'.$galeri->gambar4) ?>" style="width: 70px;height: 70px;" />
+                                      </td> 
+                                      <td width="150">
+                                        <img src="<?php echo base_url('img/galeri/'.$galeri->gambar5) ?>" style="width: 70px;height: 70px;" />
+                                      </td> 
+                                      
+                                      <td width="200">
+                                      <a href="<?php echo site_url('admin/galeri/edit/'.$galeri->id_galeri)?>" class="btn btn-info">Edit</a>
+                                   
+                                      <a onclick="deleteConfirm('<?php echo site_url('admin/galeri/delete/'.$galeri->id_galeri)?>')"
+                                      href="#!" class="btn btn-primary">Hapus</a>
+                                      </td>
+                                    </tr>
+                                  <?php endforeach; ?>
+                                </tbody>
+                              </table>
+                            </div>
 
 
 
